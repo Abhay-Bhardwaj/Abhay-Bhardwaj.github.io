9ae96db4daaabdcb2b51a9a967bc577cd888f3f4
Abhay Bhardwaj <abhaybhardwaj0008@gmail.com>
2023-10-10 23:44:30 +0530

    matlab skill added

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -119,9 +119,9 @@ export const projects = [
             "https://www.vectorlogo.zone/logos/wordpress/wordpress-icon.svg",
         },
         {
-          name: "Figma",
+          name: "Matlab",
           image:
-            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+            "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
         },
       ],
     },
